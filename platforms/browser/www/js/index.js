107775a6e8afe6b9fc31106095a8bd722a660072
Chris <mail.lamb@gmail.com>
2019-11-19 16:21:58 +0000

    V3.0.0 incl Imperial
    Imperial input included now.

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -5,17 +5,25 @@ function onDeviceReady() {
 	document.getElementById('submitBtn').addEventListener('click', getResults);
 	let dimension = document.querySelectorAll('.dimension');
 	let dimArr = Array.from(dimension);
+	let covId = document.getElementById('covId');
+	let coverage = document.getElementById('coverage');
 
 	// let units = document.querySelector('input[name="xFactor"]:checked').value;
 	document.getElementById('imperial').addEventListener('click', function(e) {
-		document.getElementById('covId').innerHTML = `ft<sup>2</sup>/gallon`;
+		covId.innerHTML = `ft<sup>2</sup>/gallon`;
+		covId.style.background = '#CED4DA';
+		coverage.style.background = '#CED4DA';
 		dimArr.forEach(function(dim) {
 			dim.innerText = 'ft';
 		});
 	});
 
 	document.getElementById('metric').addEventListener('click', function(e) {
-		document.getElementById('covId').innerHTML = `m<sup>2</sup>/litre`;
+		covId.innerHTML = `m<sup>2</sup>/litre`;
+		covId.style.background = '#E9ECEF';
+		covId.style.color = '#495057';
+		coverage.style.background = '#E9ECEF';
+		coverage.style.color = '#495057';
 		dimArr.forEach(function(dim) {
 			dim.innerText = 'm';
 		});
@@ -26,40 +34,66 @@ function onDeviceReady() {
 
 	// 1. get results
 	function getResults() {
-		let units = document.querySelector('input[name="xFactor"]:checked').value;
+		// let units = document.querySelector('input[name="xFactor"]:checked').value;
 		const resultFooter = document.getElementById('footer');
 		const height = document.querySelectorAll('.height').value;
 		const pCover = document.getElementById('pCover').value;
 
 		if (height !== '' && pCover !== '') {
-			// get the totals for the wall heights
-			let hValue = document.querySelectorAll('.height');
-			let hValArr = Array.from(hValue);
+			let h1 = document.getElementById('height1'),
+				h2 = document.getElementById('height2'),
+				h3 = document.getElementById('height3'),
+				h4 = document.getElementById('height4'),
+				h5 = document.getElementById('height5'),
+				w1 = document.getElementById('width1'),
+				w2 = document.getElementById('width2'),
+				w3 = document.getElementById('width3'),
+				w4 = document.getElementById('width4'),
+				w5 = document.getElementById('width5');
 
-			// get the totals for the wall widths
-			let wValue = document.querySelectorAll('.width');
-			let wValArr = Array.from(wValue);
+			let winh1 = document.getElementById('winHeight1'),
+				winh2 = document.getElementById('winHeight2'),
+				winh3 = document.getElementById('winHeight3'),
+				winh4 = document.getElementById('winHeight4'),
+				winh5 = document.getElementById('winHeight5'),
+				winw1 = document.getElementById('winWidth1'),
+				winw2 = document.getElementById('winWidth2'),
+				winw3 = document.getElementById('winWidth3'),
+				winw4 = document.getElementById('winWidth4'),
+				winw5 = document.getElementById('winWidth5');
 
-			// WALL TOTALS
-			let wallTotals = inputSum(hValArr) * inputSum(wValArr);
+			let wall1 = h1.value * w1.value,
+				wall2 = h2.value * w2.value,
+				wall3 = h3.value * w3.value,
+				wall4 = h4.value * w4.value,
+				wall5 = h5.value * w5.value;
 
-			// get the total for windows and doors height
-			let hWinValue = document.querySelectorAll('.win-height');
-			let hWinValArr = Array.from(hWinValue);
+			let win1 = winh1.value * winw1.value,
+				win2 = winh2.value * winw2.value,
+				win3 = winh3.value * winw3.value,
+				win4 = winh4.value * winw4.value,
+				win5 = winh5.value * winw5.value;
 
-			// get the total for windows and doors widths
-			let wWinValue = document.querySelectorAll('.win-width');
-			let wWinValArr = Array.from(wWinValue);
+			let wallTotal = wall1 + wall2 + wall3 + wall4 + wall5;
+			let winTotal = win1 + win2 + win3 + win4 + win5;
+			// TOTAL
+			let total = wallTotal - winTotal;
 
-			// WINDOW TOTALS
-
-			let windowTotals = inputSum(hWinValArr) * inputSum(wWinValArr);
 			let pCoverInt = parseInt(pCover);
 			// let pCoats = parseInt(document.getElementById('pCoats').value);
-			let pCoats = document.querySelector('input[name="coats"]:checked').value;
+			let pCoats = parseInt(
+				document.querySelector('input[name="coats"]:checked').value
+			);
 			let pCoverage = pCoverInt / pCoats;
-			let finalTotal = Math.ceil((wallTotals - windowTotals) / pCoverage);
-
+			let finalTotal = Math.ceil(total / pCoverage);
+			let square = document.getElementById('square').innerText;
+			document.getElementById('area').innerHTML = `
+			<div class="pl-5">
+			<small><strong>SUMMARY</strong></small><br>
+			<small>AREA: ${total} sq-${square}</small><br>
+			<small>COATS: ${pCoats}</small>
+			</div>
+			`;
 			document.getElementById('resMod').innerHTML = `<h1>${finalTotal}</h1>`;
 
 			let units = document.querySelector('input[name="xFactor"]:checked').value;
@@ -74,8 +108,8 @@ function onDeviceReady() {
 			}
 		} else {
 			document.getElementById('resMod').innerHTML = `	
-			<h1>0</h1>
-			`;
+				<h1>0</h1>
+				`;
 
 			pCover == '' && height == ''
 				? (resultFooter.innerText = 'NO DATA!')
@@ -84,13 +118,13 @@ function onDeviceReady() {
 	}
 
 	// add up all the areas
-	function inputSum(valArr) {
-		let sum = 0;
-		valArr.forEach(function(arr) {
-			sum += Number(arr.value);
-		});
-		return sum;
-	}
+	// function inputSum(valArr) {
+	// 	let sum = 0;
+	// 	valArr.forEach(function(arr) {
+	// 		sum += Number(arr.value);
+	// 	});
+	// 	return sum;
+	// }
 
 	// clear button
 	let clearBtn = document.getElementById('clearBtn');
