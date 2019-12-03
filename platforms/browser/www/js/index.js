ac7bf894fb92b0ad25bd50b63fab73f8a0e8027a
Chris <mail.lamb@gmail.com>
2019-12-03 12:32:04 +0000

    Banner ad
    AdMob and Tappx banner ad

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -3,7 +3,7 @@ document.addEventListener('deviceready', onDeviceReady, false);
 function onDeviceReady() {
 	// Set AdMobAds options:
 	admob.setOptions({
-		publisherId:          "ca-app-pub-3940256099942544/2934735716",  // Required
+		publisherId:          "ca-app-pub-8816517022745547~1128143402",  // Required
 		// interstitialAdId:     "ca-app-pub-8816517022745547/1209512804",  // Optional
 		tappxIdiOS:           "pub-50706-ios-8333",            // Optional
 		// tappxIdAndroid:       "/XXXXXXXXX/Pub-XXXX-Android-AAAA",        // Optional
